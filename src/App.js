fc90bcfb150db87c52d53b3e4233e6dc1ff10ab6
plajuan <pla.juan@gmail.com>
2019-10-15 20:43:57 -0300

    Constuindo seu proprio hook

--- src/App.js
+++ src/App.js
@@ -2,26 +2,16 @@ import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Clock } from './Clock';
+import { useWindowWidth } from './useWindowWidth';
 
 //https://pucminas.instructure.com/courses/1823/pages/ciclo-de-vida-de-um-componente-componentdidupdate?module_item_id=191088
 //vamos fazer uma pesquisa somente se o valor pesquisado for diferente do que já foi buscado
 function App(){
-  //variavel de estado e a função para 
-  const [isClockHidden, setIsClockHidden] = useState(false)
-
-  if(isClockHidden){
-    return <h1>Clock is hidden</h1>
-  }
-
+  const width = useWindowWidth()
   return (
-<div>
-  <button onClick={()=> setIsClockHidden(true)}>Hide</button>
-  <Clock />
-</div>
-
-  )
+    <h1>Window width is {width}</h1>
+  )  
       
-  }
-
+}
 
 export default App;
